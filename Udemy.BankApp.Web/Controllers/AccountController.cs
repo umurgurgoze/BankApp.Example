792e69d99f71a5d078d1a58d8dc45b2ecacac4b0
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-03-13 01:54:19 +0300

    Start
    Account controller built.

---
+++ Udemy.BankApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Udemy.BankApp.Web.Data.Context;
+
+namespace Udemy.BankApp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly BankContext _context;
+
+        public AccountController(BankContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create(int id)
+        {
+            var userInfo = _context.ApplicationUsers.SingleOrDefault(x => x.Id == id);
+
+            return View(userInfo);
+        }
+    }
+}
---
+++ Udemy.BankApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Udemy.BankApp.Web.Data.Context;
+
+namespace Udemy.BankApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BankContext _context;
+
+        public HomeController(BankContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.ApplicationUsers.ToList());
+        }
+    }
+}
